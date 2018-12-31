a30d5799d2227b0fd6729f0bbf3cd22461761f93
Khaled Alabssi <khaled.absi5@gmail.com>
2018-12-31 12:52:13 +0100

    Fixing the sql bug and set the connect_args to False

--- project.py
+++ project.py
@@ -20,7 +20,7 @@ APPLICATION_NAME = "Restaurant Menu Application"
 
 
 # Connect to Database and create database session
-engine = create_engine('sqlite:///restaurantmenuwithusers.db')
+engine = create_engine('sqlite:///restaurantmenuwithusers.db', connect_args={'check_same_thread': False})
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
